8f734701564a109b700736dc4144e0212e521bea
Sophia Silovsky <ssilovsky@smith.edu>
2024-02-07 00:09:53 -0500

    added update computer os method

--- computer.py
+++ computer.py
@@ -10,7 +10,7 @@ class Computer:
     price: int
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__(self, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price):
+    def __init__(self, description: str, processor_type, hard_drive_capacity, memory, operating_system, year_made, price):
         self.description = description
         self.processor_type = processor_type
         self.hard_drive_capacity = hard_drive_capacity
@@ -20,5 +20,26 @@ class Computer:
         self.price = price
 
     # What methods will you need?
-    def create_computer(description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price):
-        computer = [description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price]
+    def create_computer(self):
+        return {'description': self.description,
+                'processor_type': self.processor_type,
+                'hard_drive_capacity': self.hard_drive_capacity,
+                'memory': self.memory,
+                'operating_system': self.operating_system,
+                'year_made': self.year_made,
+                'price': self.price}
+    
+    def update_computer_os(self, computer:dict, new_os:str):
+        print("Updating computer OS information...")
+        computer['operating_system'] = new_os
+        print("Computer OS information updated!")
+        return computer
+    
+def main():
+    comp = Computer("thingy", "boop", 123, 23, "PS4", 2002, 200)
+    compu = comp.create_computer()
+    print(compu)
+    comput = comp.update_computer_os(compu, "N64")
+    print(comput)
+
+main()
