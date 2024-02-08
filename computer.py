cdb0b10f5547f86bfa5d3946cd0546edebebc0fe
Sophia Silovsky <ssilovsky@smith.edu>
2024-02-07 21:09:26 -0500

    finished computer, resale to be continued

--- computer.py
+++ computer.py
@@ -2,7 +2,6 @@ from typing import Optional
 
 class Computer:
 
-    # What attributes will it need?
     description: str
     processor_type: str
     hard_drive_capacity: int
@@ -10,8 +9,7 @@ class Computer:
     operating_system: str
     year_made: int
     price: int
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
+
     def __init__(self, description: str, processor_type:str, hard_drive_capacity:int, 
                  memory:int, operating_system:str, year_made:int, price:int):
         self.description = description
@@ -21,36 +19,31 @@ class Computer:
         self.operating_system = operating_system
         self.year_made = year_made
         self.price = price
-
-    # What methods will you need?
+    
     def create_computer(self):
         return {'description': self.description,
-                'processor_type': self.processor_type,
-                'hard_drive_capacity': self.hard_drive_capacity,
-                'memory': self.memory,
-                'operating_system': self.operating_system,
-                'year_made': self.year_made,
-                'price': self.price}
-    
-    def refurbish(self, computer: dict, new_os: Optional[str] = None):
-        if int(computer["year_made"]) < 2000:
-            computer["price"] = 0 # too old to sell, donation only
-        elif int(computer["year_made"]) < 2012:
-            computer["price"] = 250 # heavily-discounted price on machines 10+ years old
-        elif int(computer["year_made"]) < 2018:
-            computer["price"] = 550 # discounted price on machines 4-to-10 year old machines
+            'processor_type': self.processor_type,
+            'hard_drive_capacity': self.hard_drive_capacity,
+            'memory': self.memory,
+            'operating_system': self.operating_system,
+            'year_made': self.year_made,
+            'price': self.price
+    }
+
+    def refurbish(self, new_os: Optional[str] = None):
+        if int(self.year_made) < 2000:
+            self.price = 0 # too old to sell, donation only
+        elif int(self.year_made) < 2012:
+            self.price = 250 # heavily-discounted price on machines 10+ years old
+        elif int(self.year_made) < 2018:
+            self.price = 550 # discounted price on machines 4-to-10 year old machines
         else:
-            computer["price"] = 1000 # recent stuff
+            self.price = 1000 # recent stuff
 
         if new_os is not None:
-            computer["operating_system"] = new_os # update details after installing new OS
-
+            self.operating_system = new_os # update details after installing new OS
     
-def main():
-    comp = Computer("thingy", "boop", 123, 23, "PS4", 2002, 200)
-    compu = comp.create_computer()
-    print(compu)
-    comput = comp.update_computer_os(compu, "N64")
-    print(comput)
-
-main()
+    def update_price(self, new_price: int):
+        print("Changing computer's pice...")
+        self.price = new_price
+        print("Done! The computer now costs", str(new_price) + ".")
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,13 +1,61 @@
 from computer import *
-from typing import Dict, Optional
+from typing import Optional
 
 class ResaleShop:
 
-    # What attributes will it need?
-    invetory = []
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
 
-    # What methods will you need?
+    invetory = []
+    itemID:int
+
+    def __init__(self, inventory:list):
+        self.inventory = inventory
+        self.itemID = 0
+
+    
+    def buy(self, computer:dict):
+        
+        self.itemID += 1 
+        self.inventory.append(computer)
+        print(self.itemID)
+        return self.itemID
+    
+    def print_inventory(self):
+    # If the inventory is not empty
+        if self.inventory:
+            # For each item
+            for item_id in self.inventory:
+                # Print its details
+                print(f'Item ID: {item_id} : {self.inventory[item_id]}')
+        else:
+            print("No inventory to display.")
+
+    # def sell(self, computer:dict):
+    #     if computer in self.inventory:
+    #         del self.inventory[item_id]
+    #         print("Item", item_id, "sold!")
+    #     else: 
+    #         print("Item", item_id, "not found. Please select another item to sell.")
+
+    
+
+def main():
+    comp = Computer("thingy", "boop", 123, 23, "PS4", 2002, 200)
+    comp2 = Computer("thy", "bop", 1234, 243, "PS4", 2002, 2004)
+    print(vars(comp))
+    comp.refurbish("N64")
+    print(vars(comp))
+    comp.update_price(300)
+    invent = []
+    invent = ResaleShop(invent)
+    comput = comp.create_computer()
+    comp22 = comp2.create_computer()
+    invent.buy(comput)
+    invent.buy(comp22)
+    print(invent.inventory[[comput]])
+    #invent.sell(1)
+    #print(invent)
+    #invent.print_inventory()
+
+
+main()
+
